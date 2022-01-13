322d80b43837c8963492f2198a7c7cae7e545950
6otosaka9 <otosakayu460@gmail.com>
2022-01-13 17:17:39 -0500

    isi pisi

---
+++ index.js
@@ -0,0 +1,36 @@
+__path = process.cwd()
+const express = require('express');
+const app = express();
+const path = require('path');
+//const cors = require('cors')
+//onstt secure = require('ssl-express-www');
+const project = '/sdcard/6otosaka9/web/'
+const routes = require('./src/routes/api');
+
+//Settings
+app.set('views', path.join(__dirname, 'views'))
+const puerto = process.env.PORT || 8080 || 5000 || 3000
+app.enable('trust proxy');
+app.set("json spaces", 2);
+app.use(express.static("public"));
+app.set("rootP", path.join(__dirname))
+//app.use(secure);
+
+//middlewares
+
+
+//rutas
+app.use('/api', require('./src/routes/api'));
+app.use('/', require('./src/routes/routes'));
+//app.use('/', 'routes/routes');
+
+
+//static files
+
+
+//Escuchando servidor en el puerto 3000
+app.listen(puerto, () => {
+  console.log(`Servidor en puerto ${puerto}`);
+});
+
+module.exports = app
---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+
+function login() {
+  try {
+  var apiKey = document.getElementById("apiKey").value;
+  if (apikey == "6otosaka9") {
+    swal({
+  title: "Good job!",
+  text: "You clicked the button!",
+  icon: "success",
+  button: "Aww yiss!",
+})
+alert('test')
+ }
+  } catch (e) {
+    console.log(e)
+  }
+}
+console.log(apikey)
+alert('test')
